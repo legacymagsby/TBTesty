5fdcd820b475a79f6f2d93710a2ed3375b582462
Daphne <legacymagsby@yahoo.com>
2020-10-16 00:37:06 -0500

    Create test_selenium.py

---
+++ test_selenium.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os
+
+self.username = os.getenv("TESTINGBOT_KEY")
+self.access_key = os.getenv("TESTINGBOT_SECRET")
+
+
+caps = {
+ 'platform': 'Windows',
+ 'browserName': 'chrome',
+}
+
+driver = webdriver.Remote(
+    command_executor="http://%s:%s@hub.testingbot.com/wd/hub"%(self.username, self.access_key),
+    desired_capabilities=caps)
+
+
+
+driver.get("http://www.google.com")
+if not "Google" in driver.title:
+    raise Exception("Unable to load google page!")
+elem = driver.find_element_by_name("q")
+elem.send_keys("TestingBot")
+elem.submit()
+print(driver.title)
+driver.quit()
